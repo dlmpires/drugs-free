18dc8b202dd488335693509211d2d58f1ab4f9a2
Diogo Pires <diogo.pires2211@gmail.com>
2024-10-15 22:30:13 +0100

    jquery includes fixed

--- assets/js/includeFiles.js
+++ assets/js/includeFiles.js
@@ -1,11 +1,11 @@
 $(function(){
 
 
-    $('#most-deadly-drug-chart').load('../include/most-deadly-drug-chart.html');
-    $('#accident-multiplier-chart').load('../include/accident-multiplier-chart.html');
+    $('#most-deadly-drug-chart').load('../drugs-free/include/most-deadly-drug-chart.html');
+    $('#accident-multiplier-chart').load('../drugs-free/include/accident-multiplier-chart.html');
 
-    $('#drug-details-table').load('../include/drugs-details.html');
-    $('#resources-list-table').load('../include/resources-list.html');
+    $('#drug-details-table').load('../drugs-free/include/drugs-details.html');
+    $('#resources-list-table').load('../drugs-free/include/resources-list.html');
 
-    $('#popUpSections').load('../include/popup.html');
+    $('#popUpSections').load('../drugs-free/include/popup.html');
 });
